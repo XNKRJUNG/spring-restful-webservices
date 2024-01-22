9936f23ff14e5b2a8512d9468fdeadf0e89941be
cyrus-xnkrjungg <shresthacyrus@outlook.com>
2024-01-21 18:26:46 -0600

    ResponseEntity w/ location for createUser, from perspct. of CONSUMER

--- src/main/java/com/cyrus/rest/webservices/restfulwebservices/socialmedia/controller/UserController.java
+++ src/main/java/com/cyrus/rest/webservices/restfulwebservices/socialmedia/controller/UserController.java
@@ -3,8 +3,11 @@ package com.cyrus.rest.webservices.restfulwebservices.socialmedia.controller;
 import com.cyrus.rest.webservices.restfulwebservices.socialmedia.entity.User;
 import com.cyrus.rest.webservices.restfulwebservices.socialmedia.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,17 +17,25 @@ public class UserController {
     UserDaoService userService;
 
     @GetMapping("/users")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userService.findAllUsers();
     }
 
     @GetMapping("/users/{userId}")
-    public User retriveUser(@PathVariable int userId){
+    public User retriveUser(@PathVariable int userId) {
         return userService.findUserById(userId);
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User userReqObj){
-        return userService.save(userReqObj);
+    public ResponseEntity<User> createUser(@RequestBody User userReqObj) {
+
+        User savedUser = userService.save(userReqObj);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
